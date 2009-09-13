67b07fb1ef2981ba42a8d3f6e13293e29c862334
simpsonjulian <simpsonjulian@gmail.com>
2009-09-13 21:02:12 +0100

    make this one fit me model

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -1,12 +1,15 @@
 class sudo::base {
+  $admin_group = 'admin' # must change this for non-ubuntu.  I'd stick a case statement if I didn't hate them so mucn
   package {"sudo":
     ensure => installed,
   }
 
   file {"/etc/sudoers":
     ensure => present,
+    content => template('sudo/sudoers.erb'),
     owner  => root,
     group  => root,
     mode   => 440,
+    require => Package["sudo"];
   }
 }
